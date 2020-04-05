c337e3742e0bb7809155c58902fd42c2e0d05b39
tomiokario <riopf239@gmail.com>
2020-04-05 22:40:41 +0900

    add conversion for bulleted code and  tables

--- scrapbox.py
+++ scrapbox.py
@@ -2,6 +2,77 @@ import re
 import pyperclip
 
 
+#######################################################################
+# method
+#######################################################################
+
+# 通常変換を行う関数
+def normal_conversion(line):
+    # 強調
+    if '[******* ' in line:
+        line = line.replace('[******* ','# ').replace(']','\n')
+    elif '[****** ' in line:
+        line = line.replace('[****** ','# ').replace(']','\n')
+    elif '[***** ' in line:
+        line = line.replace('[***** ','# ').replace(']','\n')
+    elif '[**** ' in line:
+        line = line.replace('[**** ','## ').replace(']','\n')
+    elif '[*** ' in line:
+        line = line.replace('[*** ','### ').replace(']','\n')
+    elif '[** ' in line:
+        line = line.replace('[** ','#### ').replace(']','\n')
+    elif '[* ' in line:
+        line = line.replace('[* ','**').replace(']','**\n')
+
+
+    # 箇条書き
+    elif re.match('^ ',line) or re.match('^\t',line):
+        line = line.replace('\t',' ')
+        # 5インデント
+        line = re.sub('^     ', '\t\t\t\t- ',line)
+        # 4インデント
+        line = re.sub('^    ' , '\t\t\t- ',line)
+        # 3インデント
+        line = re.sub('^   '  , '\t\t- ',line)
+        # 2インデント
+        line = re.sub('^  '   , '\t- ',line)
+        # 1インデント
+        line = re.sub('^ '    , ' - ',line)
+    # 改行のみ
+    elif line == '\n':
+        line = line
+    # 通常文字
+    else:
+        line = line + '\n'
+
+
+    # 画像リンク
+    if '[https://gyazo.com/' in line:
+        line = line.replace(']', '/raw)' ).replace('[', '![画像](') # githubで画像を載せるには，末尾に\rawが必要
+        line = line + '\n'
+
+    # webリンク[http://~~]
+    elif '[http://' in line or '[https://' in line:
+        line = line.replace('[','').replace(']','')
+        line = line + '\n'
+    # タイトル付きwebリンク
+    elif (('http://' in line) and ('[' in line)) or (('https://' in line) and ('[' in line)):
+        line = line.replace(']',')')
+        line = re.sub(' http','](http',line)
+        line = line + '\n'
+
+    # 文字列を返す
+    return line
+
+
+
+
+
+#######################################################################
+# main
+#######################################################################
+
+
 # クリップボードの文字列を取得
 input_string = pyperclip.paste()
 # クリップボードの文字列を一行ごとに分割して配列に格納
@@ -13,7 +84,6 @@ print("----------------------------------------------------------------------")
 print(pyperclip.paste())
 print("変換します...")
 print("----------------------------------------------------------------------")
-
 
 
 # 変数初期化
@@ -32,12 +102,11 @@ for line in array:
     line = line.replace('\u3000',' ')
 
 
-
     # ソースコードの処理
     if status == 'code':
-        # ソースコードモードの終了判定
-        if line == '\n':
-            line = line.replace('\n','```\n\n')
+        # 現在のインデントが最初以下になったらコード記法の終了を判定
+        if re.match('^ *', line.replace('\t',' ')).end() <= indent_depth:
+            line = '```\n\n' + normal_conversion(line)
             status = 'normal'
     
     # 表の処理
@@ -45,11 +114,11 @@ for line in array:
         # スペースのみの行を改行文字に置換
         line = re.sub(' +\n','\n',line)
 
-        # 表モードの終了判定
-        if line == '\n':
-            line = '\n\n'
+        # 現在のインデントが最初以下になったら表記法の終了を判定
+        if re.match('^ *', line.replace('\t',' ')).end() <= indent_depth:
+            line = '```\n\n' + normal_conversion(line)
             status = 'normal'
-        # 処理
+        # 終了しない場合，表の変換処理
         else:
             line = '|' + line
             line = line.replace('\t',' | ').replace('\n',' |\n')
@@ -64,75 +133,28 @@ for line in array:
 
     # 通常の記法処理
     else:
-
-        # h1
-        if   '[******* ' in line:
-            line = line.replace('[******* ','# ').replace(']','\n')
-        elif '[****** ' in line:
-            line = line.replace('[****** ','# ').replace(']','\n')
-        elif '[***** ' in line:
-            line = line.replace('[***** ','# ').replace(']','\n')
-        # h2 ~ 4
-        elif '[**** ' in line:
-            line = line.replace('[**** ','## ').replace(']','\n')
-        elif '[*** ' in line:
-            line = line.replace('[*** ','### ').replace(']','\n')
-        elif '[** ' in line:
-            line = line.replace('[** ','#### ').replace(']','\n')
-        # 強調
-        elif '[* ' in line:
-            line = line.replace('[* ','**').replace(']','**\n')
-
-
         # ソースコードを認識
-        #elif 'code:' in line:
-        elif re.match('^code:',line):
-            line = line.replace('code:','```')
+        if re.match('^\s*code:',line):
+            # インデントを数える
+            indent_depth = re.match('^ *', line.replace('\t',' ')).end()
+            # 変換準備
             status = 'code'
+            line = re.sub('^\s*code:','```',line)
+
 
         # 表を認識
-        #elif 'table:' in line:
         elif re.match('^table:',line):
-            line = line.replace('table:','`').replace('\n','`')
+            # インデントを数える
+            indent_depth = re.match('^ *', line.replace('\t',' ')).end()
+            # 変換準備
             status = 'table'
             table_line = False
+            line = line.replace('table:','`').replace('\n','`')
 
-        # 箇条書き
-        elif re.match('^ ',line):
-            line = line.replace('\t',' ')
-            # 5インデント
-            line = re.sub('^     ', '\t\t\t\t- ',line)
-            # 4インデント
-            line = re.sub('^    ' , '\t\t\t- ',line)
-            # 3インデント
-            line = re.sub('^   '  , '\t\t- ',line)
-            # 2インデント
-            line = re.sub('^  '   , '\t- ',line)
-            # 1インデント
-            line = re.sub('^ '    , ' - ',line)
-        # 改行のみ
-        elif line == '\n':
-            line = line
-        # 通常文字
         else:
-            line = line + '\n'
+            # 通常の場合の変換
+            line = normal_conversion(line)
 
-
-        # リンクの処理
-        ## 画像の場合
-        if '[https://gyazo.com/' in line:
-            line = line.replace(']', '/raw)' ).replace('[', '![画像](') # githubで画像を載せるには，末尾に\rawが必要
-            line = line + '\n'
-
-        ## [http://~~]の場合
-        elif '[http://' in line or '[https://' in line:
-            line = line.replace('[','').replace(']','')
-            line = line + '\n'
-        ## [タイトル http://~~]の場合
-        elif (('http://' in line) and ('[' in line)) or (('https://' in line) and ('[' in line)):
-            line = line.replace(']',')')
-            line = re.sub(' http','](http',line)
-            line = line + '\n'
 
     # 表示
     print(line, end="")
